9b12aa6f17863a78425ec366ecdf66d0a8d26ded
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2024-12-24 06:49:55 +0300

    feat: complete carbon, common-character, delete digits, dream-team tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit( n ) { 
+  // length = String(n).split('').length
+  // let arr= []
+  // for(let i =0; i<length; i++){
+  //     let newArr = String(n).split('')
+  //   console.log(newArr.splice(i,1))
+  //   arr.push(newArr)
+  // }
+  // let numberArr =  arr.map((item)=> Number(item.join('')))
+  //  return numberArr.sort((a,b)=> b - a)[0]
+   
+  const numbers = String(n).split('')
+  const arrayOfNumbers = numbers.map((value, index) => {
+    modArr = [... numbers]
+    modArr.splice(index,1)
+    return Number(modArr.join(''))
+  })
+  
+  return Math.max(...arrayOfNumbers)
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function createDreamTeam( members ) {
+//   if(!Array.isArray(members)) return false
+//   const newArr = []
+//    members.forEach((name) => {
+//       if( typeof name === 'string') {
+//           newArr.push(name.trim()[0].toUpperCase())
+//       }
+//   })
+//   return newArr.sort().join('')
+// }
+
+function createDreamTeam( members ) {
+  if (!Array.isArray(members)) return false
+
+  const res =  members
+  .filter((name)=> typeof name === 'string')
+  .map((name)=> name.trim()[0].toUpperCase())
+  .sort()
+  .join('')
+
+  return res || false
 }
 
 module.exports = {
