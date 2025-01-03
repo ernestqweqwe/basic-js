24a55d4d0fb39adada291b30110adf6e2d583206
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-01-03 14:44:10 +0300

    complete

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,14 +1,14 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
+       * Given a string, return its encoding version.
+       *
+       * @param {String} str
+       * @return {String}
+       *
+       * @example
+       * For aabbbc should return 2a3bc
+       *
  */
 function encodeLine(/* str */) {
   throw new NotImplementedError('Not implemented');
